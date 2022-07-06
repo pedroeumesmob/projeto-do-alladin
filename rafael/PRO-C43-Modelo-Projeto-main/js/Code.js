ed413167945cf9ef6ecc72f3ac05391a7cabdb6d
pedroeumesmob <93227214+pedroeumesmob@users.noreply.github.com>
2022-07-05 22:24:29 -0300

    Add files via upload

---
+++ rafael/PRO-C43-Modelo-Projeto-main/js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "variavel";
+const accessCode2 = "function";
+const accessCode3 = "database";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Dica: Sempre mudando, não constante!!", 100,70)
+// adicione código para exibir o restante dos nomes e as dicas.
+    
+fill("white")
+textSize(15)
+text("O N F C T U I", 700,150)
+fill("lightblue")
+text("Dica: pode ser criada e usada varias vezes", 700,170)
+// adicione código para exibir o restante dos nomes e as dicas.
+fill("white")
+    textSize(15)
+    text("T A D A A S B E", 250,150)
+    fill("lightblue")
+    text("Dica: guarda as informação", 250,170)
+// adicione código para exibir o restante dos nomes e as dicas.
+}
